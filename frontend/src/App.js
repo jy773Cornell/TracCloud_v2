7dc14a140a327016e76295a96130304aa89b0100
Jinhong Yu <jy773@cornell.edu>
2023-02-06 22:56:46 -0500

    project init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {render} from "react-dom";
+
+function App() {
+    return (
+        <div>
+            TracCloud
+        </div>
+    )
+}
+
+export default App
