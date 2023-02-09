26d9f46025e1bf9afffaf2e3cd2b265674392eb9
yjinhong <jy773@cornell.edu>
2023-02-09 14:07:04 -0500

    layout

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import {Outlet} from "react-router-dom";
 import './index.scss'
 
 function HomePage() {
     return (
         <div>
             HomePage
-            <Outlet/>
         </div>
     )
 }
 
-export default Layout
+export default HomePage
