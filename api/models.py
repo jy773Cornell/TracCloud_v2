79da187c6f15b7cf11a23e5f1cccc77f3639b253
Jinhong Yu <jy773@cornell.edu>
2023-02-18 11:07:19 -0500

    operation model

--- api/models.py
+++ api/models.py
@@ -186,8 +186,8 @@ class Site(models.Model):
                              null=True, blank=True, on_delete=models.SET_NULL)
     crop = models.ForeignKey(verbose_name="Crop", to="Crop", to_field="cid", related_name="site_crop",
                              null=True, blank=True, on_delete=models.SET_NULL)
-    crop_year = models.IntegerField(verbose_name="Crop Year", null=True, blank=True)
-    size = models.FloatField(verbose_name="Size", null=True, blank=True)
+    crop_year = models.CharField(verbose_name="Crop Year", max_length=32, null=True, blank=True)
+    size = models.CharField(verbose_name="Size", max_length=32)
     size_unit = models.ForeignKey(verbose_name="Size Unit", to="Unit", to_field="unitid", related_name="site_unit",
                                   null=True, blank=True, on_delete=models.SET_NULL)
     gps = models.CharField(verbose_name="GPS", null=True, blank=True, max_length=128)
@@ -243,12 +243,191 @@ class Chemical(models.Model):
     label_image = models.CharField(verbose_name="Labeled Image", null=True, blank=True, max_length=128)
     imported_from = models.CharField(verbose_name="Imported From", null=True, blank=True, max_length=32)
     validated_by = models.CharField(verbose_name="Validated By", null=True, blank=True, max_length=32)
-    entered_year = models.IntegerField(verbose_name="Entered Year", null=True, blank=True)
+    entered_year = models.CharField(verbose_name="Entered Year", null=True, blank=True, max_length=32)
     is_active = models.BooleanField(verbose_name="Is Active", default=True)
     create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
 
     def __str__(self):
         return "{} (EPA NO. {})".format(self.trade_name, self.epa_reg_no)
+
+
+'''
+Operation Entity
+'''
+
+
+class Operation(models.Model):
+    opid = models.CharField(verbose_name="OPID", primary_key=True, max_length=48)
+    user = models.ForeignKey(verbose_name="User", to="User", to_field="uid", related_name="op_user",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey(verbose_name="Operation Type", to="OperationType", to_field="optid",
+                             related_name="op_type", null=True, blank=True, on_delete=models.SET_NULL)
+    datetime = models.DateTimeField(verbose_name="Operation Datetime", auto_now=True)
+    multiple_site = models.BooleanField(verbose_name="Multiple Site", default=False)
+
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return self.opid
+
+
+class OperationType(models.Model):
+    optid = models.CharField(verbose_name="OPTID", primary_key=True, max_length=48)
+    name = models.CharField(verbose_name="Type Name", max_length=128)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PurchaseRecord(models.Model):
+    prid = models.CharField(verbose_name="PRID", primary_key=True, max_length=48)
+    user = models.ForeignKey(verbose_name="User", to="User", to_field="uid", related_name="pr_user",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    opid = models.ForeignKey(verbose_name="OPID", to="Operation", to_field="opid", related_name="pr_op",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    pur_datetime = models.DateTimeField(verbose_name="Purchase Datetime", null=True, blank=True)
+    operator = models.ForeignKey(verbose_name="Operator", to="User", to_field="uid", related_name="pr_op_user",
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+    chemical = models.ForeignKey(verbose_name="Chemical", to="Chemical", to_field="chemid",
+                                 related_name="pr_chem", null=True, blank=True, on_delete=models.SET_NULL)
+    vendor = models.CharField(verbose_name="Vendor", null=True, blank=True, max_length=64)
+    amount = models.CharField(verbose_name="Amount", max_length=32)
+    cost_per_unit = models.CharField(verbose_name="Cost Per Unit", max_length=32)
+    total_cost = models.CharField(verbose_name="Total Cost", max_length=32)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.chemical, self.pur_datetime)
+
+
+class HarvestRecord(models.Model):
+    hrid = models.CharField(verbose_name="HRID", primary_key=True, max_length=48)
+    user = models.ForeignKey(verbose_name="User", to="User", to_field="uid", related_name="hr_user",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    opid = models.ForeignKey(verbose_name="OPID", to="Operation", to_field="opid", related_name="hr_op",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    har_datetime = models.DateTimeField(verbose_name="Harvest Datetime", null=True, blank=True)
+    operator = models.ForeignKey(verbose_name="Operator", to="User", to_field="uid", related_name="hr_op_user",
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+    crop = models.ForeignKey(verbose_name="Crop", to="Crop", to_field="cid", related_name="hr_crop",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    site = models.ForeignKey(verbose_name="Site", to="Site", to_field="sid", related_name="hr_site",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    planting_date = models.DateField(verbose_name="Planting Date", null=True, blank=True)
+    bloom_date = models.DateField(verbose_name="Bloom Date", null=True, blank=True)
+    hr_area = models.CharField(verbose_name="Harvest Area", max_length=32)
+    area_unit = models.ForeignKey(verbose_name="Area Unit", to="Unit", to_field="unitid",
+                                  related_name="hr_unit", null=True, blank=True, on_delete=models.SET_NULL)
+    rows = models.CharField(verbose_name="Rows", max_length=32)
+    tracing_no = models.CharField(verbose_name="Tracking No.", null=True, blank=True, max_length=128)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.site, self.har_datetime)
+
+
+class ApplicationRecord(models.Model):
+    arid = models.CharField(verbose_name="ARID", primary_key=True, max_length=48)
+    user = models.ForeignKey(verbose_name="User", to="User", to_field="uid", related_name="ar_user",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    opid = models.ForeignKey(verbose_name="OPID", to="Operation", to_field="opid", related_name="ar_op",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    app_datetime = models.DateTimeField(verbose_name="Harvest Datetime", null=True, blank=True)
+    operator = models.ForeignKey(verbose_name="Operator", to="User", to_field="uid", related_name="ar_op_user",
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+    type = models.ForeignKey(verbose_name="Application Type", to="ApplicationType", to_field="atid",
+                             related_name="ar_type", null=True, blank=True, on_delete=models.SET_NULL)
+    target = models.ForeignKey(verbose_name="Pests/Diseases", to="PestsDiseases", to_field="pdid",
+                               related_name="ar_pd", null=True, blank=True, on_delete=models.SET_NULL)
+    chemical = models.ForeignKey(verbose_name="Chemical", to="Chemical", to_field="chemid",
+                                 related_name="ar_chem", null=True, blank=True, on_delete=models.SET_NULL)
+    water_use = models.BooleanField(verbose_name="Water Use")
+    water_unit = models.ForeignKey(verbose_name="Water Unit", to="Unit", to_field="unitid",
+                                   related_name="ar_water_unit", null=True, blank=True, on_delete=models.SET_NULL)
+    application_rate = models.CharField(verbose_name="Application Rate", max_length=32)
+    rate_unit = models.ForeignKey(verbose_name="Rate Unit", to="Unit", to_field="unitid",
+                                  related_name="ar_rate_unit", null=True, blank=True, on_delete=models.SET_NULL)
+    total_amount = models.CharField(verbose_name="Total Amount", max_length=32)
+    amount_unit = models.ForeignKey(verbose_name="Amount Unit", to="Unit", to_field="unitid",
+                                    related_name="ar_amount_opu", null=True, blank=True, on_delete=models.SET_NULL)
+    total_cost = models.CharField(verbose_name="Total Cost", max_length=32)
+    site = models.ForeignKey(verbose_name="Site", to="Site", to_field="sid", related_name="ar_site",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    applied_area = models.CharField(verbose_name="Applied Area", max_length=32)
+    area_unit = models.ForeignKey(verbose_name="Amount Unit", to="Unit", to_field="unitid", related_name="ar_area_unit",
+                                  null=True, blank=True, on_delete=models.SET_NULL)
+    crop = models.ForeignKey(verbose_name="Crop", to="Crop", to_field="cid", related_name="ar_crop",
+                             null=True, blank=True, on_delete=models.SET_NULL)
+    growth_stage = models.ForeignKey(verbose_name="Growth Stage", to="CropGrowthStage", to_field="cgsid",
+                                     related_name="ar_growth_stage", null=True, blank=True, on_delete=models.SET_NULL)
+    decision_support = models.ForeignKey(verbose_name="Decision Support", to="DecisionSupport", to_field="dsid",
+                                         related_name="ar_decision_support", null=True, blank=True,
+                                         on_delete=models.SET_NULL)
+    customer = models.ForeignKey(verbose_name="Customer", to="User", to_field="uid", related_name="ar_customer_user",
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+    wind_speed = models.CharField(verbose_name="Wind Speed", null=True, blank=True, max_length=32)
+
+    direction_choices = (
+        ("1", "east"),
+        ("2", "north"),
+        ("3", "south"),
+        ("4", "west"),
+        ("5", "southwest"),
+        ("6", "northwest"),
+        ("7", "southeast"),
+        ("8", "northeast"),
+    )
+    wind_direction = models.SmallIntegerField(verbose_name="Wind Direction", null=True, blank=True,
+                                              choices=direction_choices)
+
+    average_temp = models.CharField(verbose_name="Average Temperature", null=True, blank=True, max_length=32)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.site, self.app_datetime)
+
+
+class ApplicationType(models.Model):
+    atid = models.CharField(verbose_name="ATID", primary_key=True, max_length=48)
+    name = models.CharField(verbose_name="Type Name", max_length=128)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DecisionSupport(models.Model):
+    dsid = models.CharField(verbose_name="DSID", primary_key=True, max_length=48)
+    name = models.CharField(verbose_name="Decision Support", max_length=128)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PestsDiseases(models.Model):
+    pdid = models.CharField(verbose_name="PDID", primary_key=True, max_length=48)
+    name = models.CharField(verbose_name="Pests/Diseases", max_length=128)
+    note = models.TextField(verbose_name="Note", null=True, blank=True)
+    is_active = models.BooleanField(verbose_name="Is Active", default=True)
+    create_time = models.DateTimeField(verbose_name="Create Time", auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 '''
