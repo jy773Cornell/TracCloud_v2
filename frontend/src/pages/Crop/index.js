cad50d447e16aaccf890db93b19f8abd882caeb0
yjinhong <jy773@cornell.edu>
2023-03-09 14:33:28 -0500

    crop page

--- frontend/src/pages/Crop/index.js
+++ frontend/src/pages/Crop/index.js
@@ -7,13 +7,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import {Button} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 import {useEffect} from "react";
+import {StyledButton} from "./styles";
 
 const columns = [
     {id: 'crop', label: 'Crop', minWidth: 100},
     {id: 'variety', label: 'Variety', minWidth: 100},
-    {id: 'crop_code', label: 'Crop Code', minWidth: 120,},
+    {id: 'crop_code', label: 'Crop Code', minWidth: 100,},
     {id: 'category', label: 'Category', minWidth: 100,},
     {id: 'growth_stage', label: 'Growth Stage', minWidth: 100,},
     {id: 'update_time', label: 'Update Time', minWidth: 100,},
@@ -68,47 +69,49 @@ export default function Crop(props) {
         CropListGet(props);
     }, []);
 
-    return (<div>
-        <Button variant="contained">Add</Button>
-        <Paper sx={{width: '100%', overflow: 'hidden'}}>
-            <TableContainer sx={{maxHeight: 440}}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (<TableCell
-                                key={column.id}
-                                align={column.align}
-                                style={{minWidth: column.minWidth}}
-                            >
-                                {column.label}
-                            </TableCell>))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
-                                return (<TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                    {columns.map((column) => {
-                                        const value = row[column.id];
-                                        return (<TableCell key={column.id} align={column.align}>
-                                            {column.format && typeof value === 'number' ? column.format(value) : value}
-                                        </TableCell>);
-                                    })}
-                                </TableRow>);
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-        </Paper>
-    </div>);
+    return (
+        <div>
+            <StyledButton variant="contained" startIcon={<AddIcon/>}>Add Crop</StyledButton>
+            <Paper sx={{overflow: 'hidden', margin: "0px 15px"}}>
+                <TableContainer sx={{maxHeight: 440}}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column) => (<TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{minWidth: column.minWidth, backgroundColor: '#f5f5f5', color: 'black'}}
+                                >
+                                    {column.label}
+                                </TableCell>))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => {
+                                    return (<TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (<TableCell key={column.id} align={column.align}>
+                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                            </TableCell>);
+                                        })}
+                                    </TableRow>);
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </div>
+    );
 }
--- frontend/src/pages/Crop/styles.js
+++ frontend/src/pages/Crop/styles.js
@@ -1,2 +1,12 @@
 import {styled} from "@mui/system";
+import {Button} from "@mui/material";
+import {purple} from '@mui/material/colors';
 
+
+export const StyledButton = styled(Button)(({theme}) => ({
+    margin: "15px 15px",
+    backgroundColor: purple[500],
+    '&:hover': {
+        backgroundColor: purple[700],
+    },
+}));
