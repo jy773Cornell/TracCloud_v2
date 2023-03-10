31ec00c74e0d663ff401aea1231f10ec9595a0ca
yjinhong <jy773@cornell.edu>
2023-03-10 13:51:31 -0500

    crop page

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import * as React from 'react';
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton />
+      <GridToolbarFilterButton />
+      <GridToolbarDensitySelector />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
 
 export default function HomePage() {
-    return (
-        <div>
-            HomePage
-        </div>
-    )
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 10,
+    maxColumns: 6,
+  });
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        {...data}
+        slots={{
+          toolbar: CustomToolbar,
+        }}
+      />
+    </div>
+  );
 }
