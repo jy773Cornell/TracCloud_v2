00f3d064151cf3192b0708fae6bbd727fe87e732
yjinhong <jy773@cornell.edu>
2023-03-31 11:32:36 -0400

    site page

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -9,6 +9,7 @@ import {
     Table,
     TableHeaderRow,
     TableTreeColumn,
+    TableInlineCellEditing,
 } from '@devexpress/dx-react-grid-material-ui';
 
 import {
@@ -18,9 +19,65 @@ import {
 
 const getChildRows = (row, rootRows) => (row ? row.items : rootRows);
 
-const EditButton = () => (
-    <button>Edit</button>
+const EditButton = ({onClick}) => (
+    <button onClick={onClick}>Edit</button>
 );
+
+const EditCell = ({value, onValueChange}) => {
+    const [tempValue, setTempValue] = useState(value);
+
+    const handleInputChange = (e) => {
+        setTempValue(e.target.value);
+    };
+
+    const handleSaveClick = () => {
+        onValueChange(tempValue);
+    };
+
+    return (
+        <>
+            <input type="text" value={tempValue} onChange={handleInputChange}/>
+            <button onClick={handleSaveClick}>Save</button>
+        </>
+    );
+};
+
+const EditCellComponent = ({column, row, value, onValueChange}) => {
+    const [editing, setEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setEditing(true);
+    };
+
+    const handleSaveClick = (newValue) => {
+        onValueChange(row.id, column.name, newValue);
+        setEditing(false);
+    };
+
+    if (editing) {
+        return (
+            <EditCell value={value} onValueChange={handleSaveClick}/>
+        );
+    }
+
+    return (
+        <EditButton onClick={handleEditClick}/>
+    );
+};
+
+const Cell = (props) => {
+    const {column, row, value} = props;
+
+    if (column.name === 'edit') {
+        return (
+            <EditCellComponent {...props} />
+        );
+    }
+
+    return (
+        <Table.Cell {...props} />
+    );
+};
 
 export default function Homepage() {
     const [columns] = useState([
@@ -32,10 +89,9 @@ export default function Homepage() {
             name: 'edit',
             title: '',
             getCellValue: (row) => row.id,
-            cellComponent: EditButton,
         },
     ]);
-    const [data] = useState(generateRows({
+    const [data, setData] = useState(generateRows({
         columnValues: {
             ...defaultColumnValues,
             items: ({random}) => (random() > 0.5
