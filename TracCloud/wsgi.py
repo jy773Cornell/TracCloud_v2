98c17665eb42911fde787d4ec888623e674cc98c
Jinhong Yu <jy773@cornell.edu>
2023-04-25 16:19:04 -0400

    azure setting

--- TracCloud/wsgi.py
+++ TracCloud/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TracCloud.settings')
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = 'TracCloud.production' if 'WEBSITE_HOSTNAME' in os.environ else 'TracCloud.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
