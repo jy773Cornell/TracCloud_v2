b78ed32d27c7df419039b766011e8518afe39ba4
Jinhong Yu <jy773@cornell.edu>
2023-04-25 23:01:59 -0400

    debug mode

--- TracCloud/production.py
+++ TracCloud/production.py
@@ -44,6 +44,7 @@ DATABASES = {
 # CACHES SETTING
 conn_str = os.environ['AZURE_REDIS_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!! {conn_str_params}")
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
